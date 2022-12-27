e1643828094e3b5b896ca05ca84cf47c873bd940
JR <jadedResearcher@gmail.com>
2022-12-27 10:51:43 -0500

    basic filtering works

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 window.onload = ()=>{
   renderFilter();
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  let filter = urlParams.get('filter');
+  if(filter){
+    applyFilter(filter);
+  }
+
 }
 
 
 const renderFilter = ()=>{
   const select = document.querySelector("#filter");
+  select.onchange = (e)=>{
+    applyFilter(e.target.value);
+  }
   if(controls){
     const tags = fetchTags();
     for(let tag of tags){
@@ -13,6 +23,18 @@ const renderFilter = ()=>{
       option.innerText = tag;
     }
   }
+}
+
+const applyFilter = (filter)=>{
+  const entries = document.querySelectorAll(".entry");
+  for(let entry of entries){
+    if(entry.className.includes(filter)){
+      entry.style.display ="inline-block";
+    }else{
+      entry.style.display="none";
+    }
+  }
+  updateURLParams("filter="+filter)
 }
 
 const fetchTags = ()=>{
